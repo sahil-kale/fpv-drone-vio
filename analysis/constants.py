2a09e65a0782a999d9e4ef54e29b2eeae0a715c8
Sahil Kale <sahil.kale1@gmail.com>
2025-03-18 15:42:37 -0400

    Remove the hardwareeeeee

--- analysis/constants.py
+++
@@ -1,7 +0,0 @@
-NUM_CHANNELS = 4
-
-UART_BUFFER_READ_SIZE_BYTES = 2048 * 8 * 2# maybe play with this if running into buffer issues
-BASE_SAMPLE_RATE_PCM_HZ = 16000
-SAMPLE_RATE_PCM_HZ = BASE_SAMPLE_RATE_PCM_HZ
-decode_struct_str = "<hhhhhhhh"
-LOG_DIR = "logs"
