b8d5808592dd02cabc5eb720e3408c8a06c83d40
ajsp17 <aasp1717@gmail.com>
2025-03-24 21:21:00 -0400

    minor changes

--- analysis/computer_vision.py
+++ analysis/computer_vision.py
@@ -119,7 +119,7 @@ class RatioTestFilter(FeatureMatchFilter):
     Filter matches using Lowe's ratio test
     """
 
-    def __init__(self, ratio=0.75):
+    def __init__(self, ratio=0.5):
         self.ratio = ratio
     
     def filter_matches(self, matches):
@@ -414,12 +414,7 @@ if __name__ == "__main__":
     filtered_matches1 = filter.filter_matches(matches1)
     filtered_matches2 = filter.filter_matches(matches2)
 
-    show_matches(left1, right1, filtered_matches1, l1_keypoints, r1_keypoints)
-
-    # good_matches = []
-    # for m, n in filtered_matches1:
-    #     if m.distance < 0.75 * n.distance:
-    #         good_matches.append(m)
+    # show_matches(left1, right1, filtered_matches1, l1_keypoints, r1_keypoints)
 
     StereoPair = StereoProjection("camchain-..indoor_forward_calib_snapdragon_cam.yaml")
 
@@ -429,10 +424,7 @@ if __name__ == "__main__":
     pl2, pr2 = extract_points_from_matches(filtered_matches2, l2_keypoints, r2_keypoints)
     points2 = StereoPair.triangulate_points(np.array(pl2), np.array(pr2))
 
-    # plot_3d_point_cloud(points1)
     show_points(left1, pl1, right1, pr1, points1)
-
-    # plot_3d_point_cloud(points2)
 
 
 #List of images (L & R)1, (L & R)2, (L & R)3, ... (L & R)n
